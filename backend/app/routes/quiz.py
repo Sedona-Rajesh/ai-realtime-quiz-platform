7bb23e24866d13f6d23991b441203c34ef8a675e
Sedona-Rajesh <sedonarajesh@gmail.com>
2025-12-21 23:16:17 +0530

    Add join quiz via quiz code api

--- backend/app/routes/quiz.py
+++ backend/app/routes/quiz.py
@@ -2,7 +2,7 @@ import random
 import string
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-
+from fastapi import HTTPException
 from app.database import SessionLocal
 from app.models.quiz import Quiz
 from app.schemas.quiz import QuizCreate
@@ -30,3 +30,17 @@ def create_quiz(data: QuizCreate, db: Session = Depends(get_db)):
     db.refresh(quiz)
 
     return quiz
+
+@router.get("/join/{quiz_code}")
+def join_quiz(quiz_code: str, db: Session = Depends(get_db)):
+    quiz = db.query(Quiz).filter(Quiz.quiz_code == quiz_code).first()
+
+    if not quiz:
+        raise HTTPException(status_code=404, detail="Quiz not found")
+
+    return {
+        "message": "Joined quiz successfully",
+        "quiz_id": quiz.id,
+        "title": quiz.title,
+        "description": quiz.description
+    }
