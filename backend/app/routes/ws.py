7a7e7d8cd399e023ee12a923f99408792f8f7015
Sedona-Rajesh <sedonarajesh@gmail.com>
2026-01-04 12:36:50 +0530

    add websocket broadcast for live quiz questions

--- backend/app/routes/ws.py
+++ backend/app/routes/ws.py
@@ -2,26 +2,27 @@ from fastapi import WebSocket, APIRouter
 
 router = APIRouter()
 
-# This stores quiz_code -> list of users
+# quiz_code -> list of connected users
 active_connections = {}
 
 @router.websocket("/ws/{quiz_code}")
 async def quiz_websocket(websocket: WebSocket, quiz_code: str):
     await websocket.accept()
 
-    # Add user to quiz room
     if quiz_code not in active_connections:
         active_connections[quiz_code] = []
 
     active_connections[quiz_code].append(websocket)
 
-    # Send confirmation
     await websocket.send_text(f"Joined quiz room {quiz_code}")
 
     try:
         while True:
-            data = await websocket.receive_text()
-            # (we'll handle messages later)
+            message = await websocket.receive_text()
+
+            # Broadcast message to everyone in the quiz
+            for connection in active_connections[quiz_code]:
+                await connection.send_text(message)
+
     except:
-        # Remove user when disconnected
         active_connections[quiz_code].remove(websocket)
